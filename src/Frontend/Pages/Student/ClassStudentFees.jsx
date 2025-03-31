2ad8677cfd003047b5cc2e5771577807eabd8d3f
Kunal <kunalpaul673@gmail.com>
2025-04-01 04:43:36 +0530

    ClassStudent fee changed

--- src/Frontend/Pages/Student/ClassStudentFees.jsx
+++ src/Frontend/Pages/Student/ClassStudentFees.jsx
@@ -750,7 +750,7 @@ dispatch(setConfirmRequest(false))
       field: 'status',
       headerName: 'Status',
       renderCell: (row) => (
-        <div className={`px-2 py-1 rounded-full text-sm ${
+        <div className={`px-2 py-2 flex items-center lg:py-1 rounded-lg lg:rounded-full text-sm ${
           row.status === 'paid' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
         }`}>
           {row.status || '-'}
